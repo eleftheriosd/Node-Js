cf4fb16be370c8bd2b8a637c9a9110802aacc700
Lefteris <eleftherios.davros@gmail.com>
2019-01-10 10:42:04 +0200

    Node Js project

---
+++ node_passport/routes/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const router = express.Router();
+const {esnureAuthenticated} = require('../config/auth');
+
+//Welcome Page
+router.get('/',(req,res) => res.render('welcome'));
+
+//Dashboard
+router.get('/dashboard',esnureAuthenticated, (req,res) =>
+ res.render('dashboard',{
+    name:req.user.name
+ }));
+    
+module.exports = router ;
